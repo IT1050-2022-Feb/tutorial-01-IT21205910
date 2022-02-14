6fe94e8126557daff9fe26383222d840f398cb70
IT21205910 <99656458+IT21205910@users.noreply.github.com>
2022-02-14 11:57:35 +0000

    completed

--- Tute04.c
+++ Tute04.c
@@ -4,16 +4,46 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
-#include <stdio.h>
+#include<iostream>
 
-int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
-   return 0;
+using namespace std;
+
+int minimum(int n1,int n2);
+int maximum(int n1,int n2);
+int multiply(int n1,int n2);
+
+int main()
+{
+	int no1,no2;
+	
+	cout<<"Enter a value for no 1 :";
+	cin>>no1;
+	cout<<"Enter a value for no 2 :";
+	cin>>no2;
+	cout<<minimum(no1,no2)<<endl;
+	cout<<maximum(no1,no2)<<endl;
+	cout<<multiply(no1,no2)<<endl;
+		
+	return 0;
+	
+}
+int minimum(int n1,int n2){
+	if(n1<n2){
+		return n1;
+	}
+	else{
+		return n2;
+	}
+}
+int maximum(int n1,int n2){
+	if(n1<n2){
+		return n2;
+	}
+	else{
+		return n1;
+	}
+}
+int multiply(int n1,int n2){
+		return n1*n2;
+	
 }
